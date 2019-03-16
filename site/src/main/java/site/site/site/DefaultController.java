6e28c4d98dca7c3dc5bcb9d08d6458b65d0c169c
Aaron Tello-Wharton <aaron.tkd@hotmail.com>
2019-03-16 12:40:24 +0000

    Thymeleaf working

---
+++ site/src/main/java/site/site/site/DefaultController.java
@@ -0,0 +1,14 @@
+package site.site.site;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DefaultController {
+
+    @RequestMapping("/")
+    public String defaultRequest(){
+        return "index";
+    }
+}
