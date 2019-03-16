cc374e2802468f80e2aeed7d5a6b664421322c6d
Aaron Tello-Wharton <aaron.tkd@hotmail.com>
2019-03-16 18:04:53 +0000

    now you can login

--- site/src/main/java/site/site/site/LoginController.java
+++ site/src/main/java/site/site/site/LoginController.java
@@ -1,6 +1,8 @@
 package site.site.site;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import site.site.site.model.state.StateVersion;
 import site.site.site.model.users.User;
@@ -9,8 +11,10 @@ import site.site.site.model.users.User;
 public class LoginController {
 
     @PostMapping("/login")
-    public void login(User user)
+    public User login(@RequestBody User user)
     {
         StateVersion.getStateVersion(state -> state.addUser(user));
+
+        return user;
     }
 }
