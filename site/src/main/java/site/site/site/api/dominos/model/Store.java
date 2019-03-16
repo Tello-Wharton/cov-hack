c1e648a094a8ce479dda3fc04db72bb5eedc0915
Tello-Wharton <aaron.tkd@hotmail.com>
2019-03-16 13:48:25 +0000

    Callum: Start creating api model

---
+++ site/src/main/java/site/site/site/api/dominos/model/Store.java
@@ -0,0 +1,16 @@
+package site.site.site.api.dominos.model;
+
+public class Store {
+    public long id;
+    public String name;
+    public double latitude;
+    public double longitude;
+    public long regionId;
+    public String postCode;
+    public boolean isOpen;
+    public boolean isCollectionAvaileFromStore;
+    public String franchiseeName;
+    public String storeStatusMessage;
+    public String welcomeMessage;
+    public long menuVersion;
+}
