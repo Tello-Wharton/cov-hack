9351583bcac7c553683543920c4790d0104caae3
Spencer Yeboah-Mensah <spencer.mensah98@gmail.com>
2019-03-17 02:13:32 +0000

    Oject Ready for WebSocket

--- ReactFront/src/Pages/Carding.js
+++ ReactFront/src/Pages/Carding.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody,
   CardTitle, CardSubtitle, Button } from 'reactstrap';
+import axios from 'axios';
+
 
 class Carding extends Component {
   constructor(props) {
       super(props);
   }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+
+    const pizzaorder = {
+      pizzaid: this.props.id,
+      pizzaname: this.props.title,
+      pizzaprice: this.props.price
+    }
+
+    console.log(pizzaorder);
+
+    // const test = new WebSocket("ws://localhost:8080/state-updater");
+    // test.onopen = function() {
+    //   console.log("YAY")
+    //   setInterval(() => {
+    //     test.send("FUCK YEAH!");
+    //     console.log("FUCK YEAH!");
+    //   }, 1000 );
+    // };
+  }
+
+
   render() {
     return (
       <div>
@@ -15,7 +40,7 @@ class Carding extends Component {
             <CardSubtitle>{this.props.type}</CardSubtitle>
             <div className="card-footer">
               <CardText>{this.props.price}</CardText>
-              <Button>🛒</Button>
+              <Button onClick={(e) => this.onSubmit(e)}>🛒</Button>
             </div>
           </CardBody>
         </Card>
