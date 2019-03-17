9eed3aacc8fb56a14b066ca1455e11906f75137e
Aaron Tello-Wharton <aaron.tkd@hotmail.com>
2019-03-17 02:15:40 +0000

    added functions to state

--- site/src/main/java/site/site/site/model/state/State.java
+++ site/src/main/java/site/site/site/model/state/State.java
@@ -1,8 +1,10 @@
 package site.site.site.model.state;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import site.site.site.model.users.User;
 
 import java.util.*;
+import java.util.function.BiConsumer;
 
 public class State {
 
@@ -14,16 +16,37 @@ public class State {
         return instance;
     }
 
+    @JsonIgnore
+    private Map<String, BiConsumer<State, String>> updateFunctions;
 
     private Map<String, User> users;
+    private Map<String, String> cake;
 
 
     private State() {
         this.users = new HashMap<>();
+        this.updateFunctions = updateFunctions();
     }
 
     public void addUser(User user)
     {
         this.users.put(user.username, user);
     }
+
+    public void addTest(String string){
+        cake.put(string, string);
+    }
+
+    private static HashMap<String, BiConsumer<State, String>> updateFunctions(){
+
+        var updateFunctions = new HashMap<String, BiConsumer<State, String>>();
+
+        BiConsumer<State, String> test = (state, string) -> state.addTest(string);
+
+        updateFunctions.put("test", test);
+
+        return updateFunctions;
+    }
+
+
 }
